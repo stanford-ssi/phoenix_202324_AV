e28a2a00ef8dedb14cf22fd023d0b872aa160202
Annabelle Jayadinata <abellej@stanford.edu>
2024-04-04 22:52:58 -0700

    fixed

---
+++ Phoenix-payload/README.md
@@ -0,0 +1,2 @@
+# Phoenix-payload
+Phoenix Payload
---
+++ Phoenix-payload/camera.py
@@ -0,0 +1,9 @@
+#https://raspberrypi-guide.github.io/electronics/image-and-video-recording
+
+import picamera
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.start_recording('my_video.h264')
+camera.wait_recording(60)
+camera.stop_recording()
